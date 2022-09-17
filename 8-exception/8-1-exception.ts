298ce0bf256693e469933660eb9d53dee0ea26c0
Changuk <devchoi1213@gmail.com>
2022-09-17 23:07:55 +0900

    exception

---
+++ 8-exception/8-1-exception.ts
@@ -0,0 +1,27 @@
+// Java: Exception
+// JavaScript: Error
+// Error(Exception) Handling: try -> catch -> finally
+
+function readFile(fileName: string): string {
+    if(fileName === 'not exist') {
+        throw new Error(`file not exist! ${fileName}`);
+    }
+    return 'file contents';
+}
+
+function closeFile(fileName: string) {}
+
+function run() {
+    const fileName = 'not exist';
+
+    try {
+        console.log(readFile(fileName));
+    } catch (error) {
+        console.log('catched!');
+        return;
+    } finally {
+        closeFile(fileName);
+        console.log('closed!')
+    }
+}
+run();
