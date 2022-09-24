f8e2fe8089f625935397ecb61400465004942564
Changuk <devchoi1213@gmail.com>
2022-09-24 16:29:37 +0900

    Initial setting for Motion project

--- 11-config/build/main.js
+++ 11-config/build/main.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('hello!');
+{
+    class Car {
+        constructor() {
+            this.engine = 0;
+        }
+        move() {
+            const engine = this.engine + 1;
+            console.log(engine);
+        }
+    }
+    const car = new Car();
+    car.move();
+}
+//# sourceMappingURL=main.js.map
